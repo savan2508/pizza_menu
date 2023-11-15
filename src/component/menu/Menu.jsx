326a9dfdd628a5997f2c70fc9d9edc2838f69c03
Savan <sawanpatel2508@gmail.com>
2023-11-14 22:43:30 -0500

    Update the pizza menu style

--- src/component/menu/Menu.jsx
+++ src/component/menu/Menu.jsx
@@ -5,9 +5,15 @@ export const Menu = () => {
   return (
     <main className="menu">
       <h3>Our Menu</h3>
-      {pizzaData.map((pizza) => (
-        <Pizza key={pizza.id} pizza={pizza} />
-      ))}
+      <p>
+        Authentic Italian cuisine. 6 creative dishes to choose from. All from
+        our stone oven, all organic, all delicious.
+      </p>
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza key={pizza.id} pizza={pizza} />
+        ))}
+      </ul>
     </main>
   );
 };
--- src/component/pizza/Pizza.jsx
+++ src/component/pizza/Pizza.jsx
@@ -1,14 +1,14 @@
 export const Pizza = ({ pizza }) => {
-  const { name, photoName, ingredients, price } = pizza;
+  const { name, photoName, ingredients, price, soldOut } = pizza;
 
   return (
-    <div className="pizza">
+    <li className={`pizza ${soldOut ? "sold-out" : ""}`}>
       <img src={photoName} alt={`${name}`} />
       <div>
         <h3>{name}</h3>
         <p>{ingredients}</p>
-        <span>${price}</span>
+        <span>{soldOut ? "SOLD OUT" : "$" + price}</span>
       </div>
-    </div>
+    </li>
   );
 };
